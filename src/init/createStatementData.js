d4802f1c3197303925613cb9f385f9d8690f1929
hs.y <18284190+HeesunYoon@users.noreply.github.com>
2022-11-12 16:17:55 +0900

    조건부 로직을 객체지향 문법으로 변경

--- src/init/createStatementData.js
+++ src/init/createStatementData.js
@@ -1,62 +1,61 @@
+class PerformanceCalculator {
+    constructor(aPerformance, aPlay){
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount(){
+        let result = 0;
+        switch(this.play.type){
+            case "tragedy": //비극
+            result = 40000;
+            if (this.performance.audience > 30){
+                result += 1000 * (this.performance.audience - 30);
+            } 
+            break;
+        
+            case "comedy": //희극
+            result = 30000;
+            if (this.performance.audience > 20){
+                result += 1000 + 500 * (this.performance.audience - 20);
+            }
+            result += 300 * this.performance.audience;
+            break;
+            default:
+            throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+        return result;
+    }
+}
 
 // 중간 데이터 생성을 전담한다.
-
 export default function createStatementData(invoice, plays){
     const result = {};
     result.customer = invoice.customer;
     result.performances = invoice.performances.map(enrichPerformance);
-    // 총합
     result.totalAmount = totalAmount(result);
     result.totalVolumeCredits = totalVolumeCredits(result);
     return result;
-  
+    
+    /**
+     * 각 공연의 정보를 중간 데이터 구조에 채워준다.
+    */
     function enrichPerformance(aPerformance){
-      // 얕은 복사 수행
-      /**
-       * 가변 데이터는 금방 상하기(?) 때문에 불변으로 만들기 위해 
-       * 얕은 복사를 수행해서 데이터를 채운다.
-      */
+      // ECMAScript2015(ES6) 객체지향 구조를 활용한다.
+      const calculator = new PerformanceCalculator(aPerformance);
       const result = Object.assign({}, aPerformance);
       result.play = playFor(result);
       result.amount = amountFor(result);
-      // 적립 포인트 계산 함수 옮김
       result.volumCredits = volumeCreditsFor(result);
       return result;
     }
   
-    // renderPlainText 의 중첩함수였던 playFor을 statement로 옮긴다.
     function playFor(aPerformance) {
       return plays[aPerformance.playID];
     }
   
     function amountFor(aPerformance){
-      /*
-      자바스크립트와 같은 동적 타입 언어는 타입이 드러나게 작성한다.
-      매개변수 이름에 접두어로 타입 이름을 적고 역할이 뚜렷하지 않을 때는
-      부정 관사 (a/an)를 붙인다.
-      Smalltalk Best Practice Patterns 참고
-      */
-      let result = 0;
-      switch (aPerformance.play.type) {
-        case "tragedy": //비극
-        result = 40000;
-          if (aPerformance.audience > 30){
-            result += 1000 * (aPerformance.audience - 30);
-          } 
-          break;
-    
-        case "comedy": //희극
-        result = 30000;
-          if (aPerformance.audience > 20){
-            result += 1000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-          
-        default:
-          throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-      }
-      return result;
+      return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
     }
   
     function volumeCreditsFor(aPerformance) {
@@ -69,19 +68,13 @@ export default function createStatementData(invoice, plays){
   
     // <-- 총합 시작 
     function totalAmount(data){
-      let result = 0;
-      for (let perf of data.performances){
-        result += perf.amount;
-      }
-      return result;
+        return data.performances
+        .reduce((total, p) => total + p.amount, 0);
     }
   
     function totalVolumeCredits(data){
-      let result = 0;
-      for (let perf of data.performances){
-        result += perf.volumCredits;
-      }
-      return result;
+        return data.performances
+        .reduce((total, p) => total + p.volumCredits, 0);
     }
     // 총합 끝 --> 
   
--- src/init/index.js
+++ src/init/index.js
@@ -3,37 +3,33 @@ import PLAYS from "../play.json";
 import createStatementData from "./createStatementData";
 
 // format과 같이 함수 변수를 일반 함수로 변경하는 것도 리팩토링에 해당된다.
-function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+function htmlStatement(invoice, plays) {
+  return renderHTML(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data, plays){
-  // invoice와 plays와 같은 인수들을 중간 데이터 구조로 옮기면
-  // 단계를 줄일 수 있다.
-  let result = `청구 내역(고객명: ${data.customer})\n`;
+function renderHTML(data, plays){
+  let result = `<h1>청구 내역(고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>죄석 수</th><th>금액</th></tr>";
   for (let perf of data.performances) {
     // 청구 내역을 출력한다.
-    result += `${perf.play.name}: ${usd(perf.amount)} (${
-      perf.audience}석)\n`;
+    result += ` <tr><td>${perf.play.name}</td><td>(${
+      perf.audience}석)</td>\n`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  result += "</table>";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: ${data.totalVolumeCredits}점</p>\n`;
   return result;
-
-  /*
-  format은 함수가 하는 일을 충분히 설명해주지 못한다.
-  formatAsUSD는 너무 장황하므로 함수의 핵심인 화폐 단위 맞추기를 생각해서
-  usd라고 이름짓는다.
-  함수를 쪼개는 리팩터링은 이름을 잘 지어야만 효과가 있다.
-  */
-  function usd(aNumber){
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber/100);
-  }
-  
 }
 
-console.log(statement(INVOICE[0], PLAYS));
+// 최상위로 이동
+function usd(aNumber){
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber/100);
+}
+
+console.log(htmlStatement(INVOICE[0], PLAYS));
