4e42d31dfc4aa862682bc2f935b29a170a01d1ed
hs.y <grapewater@daum.net>
2022-12-05 15:47:26 +0900

    Chapter04 4.2 테스트할 샘플 코드

---
+++ Chapter04/Province.js
@@ -0,0 +1,47 @@
+function sampleProvinceData(){
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10, production: 9},
+            {name: "Attalia", cost: 12, production: 10},
+            {name: "Sinope", cost: 10, production: 6}
+        ],
+        demand: 30,
+        price: 20
+    }
+}
+
+class Province {
+
+    constuctor(doc){
+        this._name = doc.name;
+        this._producers = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
+    }
+    
+    addProducer(arg){
+        this._producers.push(arg);
+        this._totalProduction += arg.production;
+    }
+
+    get name(){ return this._name; }
+    get producers() { return this._producers.slice(); }
+    get totalProduction() { return this._totalProduction; }
+    set totalProduction(arg) { this._totalProduction = arg; }
+    get demand() { return this._demand; }
+    set demand(arg) { return this._demand = parseInt(arg); } // 숫자로 파싱해서 저장
+    get price() { return this._price; }
+    set price(arg) { this._price = parseInt(arg); } // 숫자로 파싱해서 저장
+
+}
+
+
+
+
+
+
+
+
